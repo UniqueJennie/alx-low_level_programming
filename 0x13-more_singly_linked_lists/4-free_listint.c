85e21a9415e508b8e9ab7208875ead98cb6156e6
Ekekwe Jennifer <jennifersaidso@gmail.com>
2023-08-02 04:05:49 -0700

    :smile:C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - will free linked list
+ * @head: listint_ the list to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
